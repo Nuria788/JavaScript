6b15e44e1bc4b1e16955cd1d2f7f082334f61adf
Nuria788 <200093@iessierradeguara.com>
2022-03-21 12:56:54 +0100

    anadido Conexion

---
+++ CONEXION_CLASE.COM/ejer5.php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class ="superior">
+        <form action="crearUsuario.php" method="post">
+            <h1> Formulario para crear usuario</h1>
+            <p> Usuario     <input type="text" name="usuario" ></p>
+            <p> Contraseña  <input type="password" name="contrasena" ></p>
+            <p> DNI         <input type="text" name="dni" ></p>
+            <p> Nombre      <input type="text" name="nombre" ></p>
+            <p> Apellidos   <input type="text" name="apellidos" ></p>
+            <input type="submit" value="Crear">
+
+        </form>
+    </div>
+
+    <div class ="inferior">
+        <form action="login.php" method="post">
+            <h1> Formulario para hacer login</h1>
+            <p> Usuario     <input type="text" name="usuario" ></p>
+            <p> Contraseña  <input type="password" name="contrasena" ></p>
+            <input type="submit" value="Login">
+        </form>
+
+    </div>
+
+    
+</body>
+</html>
