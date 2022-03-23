6118dc12c8eff231de922140b14289eb762bbceb
Nuria788 <200093@iessierradeguara.com>
2022-03-23 12:18:30 +0100

    .

--- CONEXION_CLASE.COM/Ej3.php/login1.php
+++ CONEXION_CLASE.COM/Ej3.php/login1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +7,57 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php      
+    $altura   = $_POST["altura"];
+    $peso = $_POST["peso"];
+    $edad    = $_POST["edad"];
+  
+    $servidor = "localhost";
+    $usuarioBD= "nuria";
+    $contrasenyaBD ="nuria";
+    $db = "NM";
+    $tabla = "tablaNM";  
+        
+        
+        
+$conexion = mysqli_connect("localhost", $usuarioBD, $contrasenyaBD, $db);
+
+if (!$conexion) {
+    alert("Conexion fallida: " . mysqli_connect_error());
+}else{
+    alert("Conectado correctamente");
+}   //  SIEMPRE ES ASI
+      
+        $seleccionar = "SELECT INTO  \"$tabla\"  
+        WHERE altura= \"$altura\" AND peso=\"$peso\" AND edad=\"$edad\"  ";    
+
+        $datos = mysqli_query($conexion, $seleccionar);
+        
+        if($datos){
+
+            $fila = mysqli_fetch_assoc($datos);
+
+            $usuario = $fila["usuario"];
+            $genero = $fila["genero"];
+            $union = "<h3>Nombre: $usuario genero: $genero</h3>"{
+                for ($i=1; $i<3; $i= $i + 1)
+                {
+                    echo "<h3> $i. Nombre: $usuario genero: $genero</h3>"
+                    }
+                };
+           
+
+
+        };
+
+        
+        
+
+
+       
+        
+    ?>
 
 
 
